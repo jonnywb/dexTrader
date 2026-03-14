bc9a628a816f97b71b7ae0b6d95b6ac8dd25f8b5
Jonathan Brierley <jonnywb@users.noreply.github.com>
2026-03-14 00:33:19 +0000

    login functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
+import { JwtPayload } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import "react-native-url-polyfill/auto";
+import Auth from "./components/Auth";
+import { supabase } from "./lib/supabase";
 
-export default function Index() {
+export default function App() {
+  const [claims, setClaims] = useState<JwtPayload | null>(null);
+
+  useEffect(() => {
+    const fetchClaims = async () => {
+      const { data } = await supabase.auth.getClaims();
+      setClaims(data?.claims ?? null);
+    };
+    fetchClaims();
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(() => {
+      fetchClaims(); // Fixed: removed the trailing (
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Auth />
+      {claims && <Text>{claims.sub}</Text>}
     </View>
   );
 }
